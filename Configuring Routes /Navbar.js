d9ec6de2382662fd11802663a9b44fd3cda3c9f9
Aditya Dwivedi <104710359+ADITYADWIVEDI18@users.noreply.github.com>
2024-01-07 15:06:51 +0530

    Create Navbar.js

---
+++ Configuring Routes /Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import '../App.css';
+function Navbar() {
+  return (
+    <div>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
